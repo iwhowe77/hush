89be692ff285376e03d889468063bcdf5a8e6429
nandini10 <nandini2@illinois.edu>
2019-11-19 16:27:11 -0600

    login ui finished

---
+++ app/src/main/java/com/example/hush/PasswordCreation.java
@@ -0,0 +1,14 @@
+package com.example.hush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasswordCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_creation);
+    }
+}
